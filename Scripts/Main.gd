9b63c629885bac43f79e72e9369a61bc14011c85
xeltux <xeltuxofficial@gmail.com>
2023-04-29 21:41:34 +0530

    Minor updates for the day
    Note to self: Map recepies list with new list of names

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,7 +3,14 @@ extends Node2D
 var jmenu_in_use = false
 var imenu_in_use = false
 
-# Called when the node enters the scene tree for the first time.
+var veggies = []
+
+var recipie1 = ['carrot', 'cheese', 'pennepasta'] # Tutorial
+var recipie2 = ['bellpepper', 'cauliflower', 'onion', 'ricevinegar', 'sesameoil', 'soysauce'] # Stir Fried Cauliflower
+var recipie3 = ['sage', 'filleted salmon', 'garlicpowder', 'oliveoil', 'salt']
+
+var recipies = ['recipie1', 'recipie2', 'recipie3']
+
 func _ready():
 	DialogueManager.show_example_dialogue_balloon(load("res://Assets/Dialog/tutorial.dialogue"), "start")
 	$Menu.visible = false
@@ -11,14 +18,12 @@ func _ready():
 var menuIsBeingUsed = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		DialogueManager.show_example_dialogue_balloon(load("res://Assets/Dialog/tutorial.dialogue"), "start")
-	pass
+
 
 
 func _on_menu_button_pressed():
@@ -28,9 +33,6 @@ func _on_menu_button_pressed():
 	else:
 		$Menu.visible = false
 		menuIsBeingUsed = false
-
-func _on_pot_body_entered(body):
-	body.queue_free()
 
 
 func _on_ingredients_toggled(button_pressed):
@@ -45,3 +47,26 @@ func _on_journal_toggled(button_pressed):
 		$JournalMenu.visible = true
 	else:
 		$JournalMenu.visible = false
+
+
+func _on_pot_area_entered(area):
+	var body_name = area.get_name()
+	veggies.append(body_name)
+	area.queue_free()
+	for i in range(recipies.size()):
+		var current_array = recipies[i]
+		if arrays_match(veggies, current_array) == true:
+			pass
+		else:
+			pass
+func arrays_match(array1, array2):
+	if array1.size() != array2.size():
+		return false
+	var sorted_array1 = array1.duplicate()
+	sorted_array1.sort()
+	var sorted_array2 = array2.duplicate()
+	sorted_array2.sort()
+	for i in range(sorted_array1.size()):
+		if sorted_array1[i] != sorted_array2[i]:
+			return false
+	return true
