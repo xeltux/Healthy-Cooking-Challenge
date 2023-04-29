0469be1d3757ce11265bc6cae0f9b9f6d4b58dea
xeltux <xeltuxofficial@gmail.com>
2023-04-29 15:54:56 +0530

    Cutting effect added

--- Scripts/Ingredients.gd
+++ Scripts/Ingredients.gd
@@ -14,5 +14,6 @@ func _process(delta):
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
+		$Cut.play()
 		$Normal.visible = false
 		$Chopped.visible = true
