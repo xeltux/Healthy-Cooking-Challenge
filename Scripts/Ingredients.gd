c2efb2bc013a6b98c9861702dd0aa8373a69b4df
xeltux <xeltuxofficial@gmail.com>
2023-04-29 15:08:11 +0530

    Added First Ingredient

---
+++ Scripts/Ingredients.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Normal.visible = true
+	$Chopped.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		$Normal.visible = false
+		$Chopped.visible = true
