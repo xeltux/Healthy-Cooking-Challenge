b9d878fd9fba54da9cd3c29a8e34bc7894473fb4
xeltux <xeltuxofficial@gmail.com>
2023-04-29 16:43:50 +0530

    Updated Dialog

--- Scripts/Ingredients.gd
+++ Scripts/Ingredients.gd
@@ -12,7 +12,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_touch"):
 		if dragging == false:
-			get_tree().get_root().position = get_viewport().get_mouse_position()
+			$Thing.position = get_viewport().get_mouse_position()
 
 func _on_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
