468659b637dc3eb1fbf5911c98d7a234e4d84f92
xeltux <xeltuxofficial@gmail.com>
2023-04-29 20:43:17 +0530

    Working Buttons, Updates

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var jmenu_in_use = false
+var imenu_in_use = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DialogueManager.show_example_dialogue_balloon(load("res://Assets/Dialog/tutorial.dialogue"), "start")
@@ -27,6 +30,18 @@ func _on_menu_button_pressed():
 		menuIsBeingUsed = false
 
 func _on_pot_body_entered(body):
-	var root_node = body.get_node()
-	var root_node_name = root_node.get_parent().get_name()
-	print(root_node_name)
+	body.queue_free()
+
+
+func _on_ingredients_toggled(button_pressed):
+	if button_pressed == true:
+		$IngredientsMenu.visible = true
+	else:
+		$IngredientsMenu.visible = false
+
+
+func _on_journal_toggled(button_pressed):
+	if button_pressed == true:
+		$JournalMenu.visible = true
+	else:
+		$JournalMenu.visible = false
